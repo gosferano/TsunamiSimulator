ddc7822d6c8c24b9f1c2183819dd708337652d86
Gytis Barkauskas <gytisbar@protonmail.com>
2017-01-20 22:18:21 +0200

    Added InputController.cs

---
+++ TsunamiSimulatorUnity/Assets/Scripts/Controllers/InputController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : Singleton<InputController>
+{
+
+    public static float moveValue;
+    public static float jumpValue;
+    public static float slamValue;
+    public static float exitValue;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        computeInput();
+    }
+
+    void computeInput()
+    {
+        exitValue = 0;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            exitValue = 1;
+        }
+
+        moveValue = 0;
+        jumpValue = 0;
+        slamValue = 0;
+
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            moveValue = Input.GetAxis("Horizontal");
+            if (Input.GetAxis("Jump") > 0) jumpValue = 1;
+            if (Input.GetAxis("Slam") > 0) slamValue = 1;
+        }
+    }
+}
