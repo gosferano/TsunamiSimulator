79c5e44a6236c66cec5a13a53978901a66ad7758
Gytis Barkauskas <gytisbar@protonmail.com>
2017-01-21 02:41:13 +0200

    Placeholder street and building prefabs, white sprite, fixed movement, added Street.cs

--- TsunamiSimulatorUnity/Assets/Scripts/Player/Player.cs
+++ TsunamiSimulatorUnity/Assets/Scripts/Player/Player.cs
@@ -5,33 +5,68 @@ using UnityEngine;
 public partial class Player : MonoBehaviour {
 
     public Vector3 movementVector;
+    public bool canTurnUp;
+    public bool canTurnDown;
+    public bool canTurnLeft;
+    public bool canTurnRight;
 
 	// Use this for initialization
 	void Start () {
-        movementVector = new Vector2(velocity, 0);
+        movementVector = new Vector3(velocity, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         Input();
         Move();
-        Debug.Log(movementVector);
 	}
 
     void Input()
     {
         if (movementVector.x != 0 && InputController.moveValueY != 0)
         {
-            movementVector = new Vector2(0, InputController.moveValueY * velocity);
+            if ((InputController.moveValueY > 0 && canTurnUp) || (InputController.moveValueY < 0 && canTurnDown))
+            {
+                movementVector = new Vector3(0, InputController.moveValueY * velocity, 0);
+                transform.position = new Vector3(Mathf.Round(transform.position.x), transform.position.y, transform.position.z);
+            }
         }
-        else if (movementVector.y != 0 && InputController.moveValueX != 0)
+        else if (movementVector.y != 0 && InputController.moveValueX != 0 && (canTurnLeft || canTurnRight))
         {
-            movementVector = new Vector2(InputController.moveValueX * velocity, 0);
+            if ((InputController.moveValueX > 0 && canTurnRight) || (InputController.moveValueX < 0 && canTurnLeft))
+            {
+                movementVector = new Vector3(InputController.moveValueX * velocity, 0, 0);
+                transform.position = new Vector3(transform.position.x, Mathf.Round(transform.position.y), transform.position.z);
+            }
         }
     }
 
     void Move()
     {
-        transform.position += movementVector;
+        transform.position += movementVector * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        if (collision.gameObject.tag == "Street")
+        {
+            Street street = collision.gameObject.GetComponent<Street>();
+            canTurnRight = street.turnRight;
+            canTurnUp = street.turnUp;
+            canTurnLeft = street.turnLeft;
+            canTurnDown = street.turnDown;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Street")
+        {
+            canTurnDown = false;
+            canTurnUp = false;
+            canTurnLeft = false;
+            canTurnRight = false;
+        }
     }
 }
