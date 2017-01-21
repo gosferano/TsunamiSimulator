8ac3bb287a1b9b5d08b7f22c223156284a926666
simonamockute <simonammm@gmail.com>
2017-01-21 05:26:25 +0200

    Added HealthBar

---
+++ TsunamiSimulatorUnity/Assets/Scripts/UI/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+    public Image Bar;
+    public float MaxHealth = 100f;
+    public float CurrentHealth = 0f;
+    
+	// Use this for initialization
+	void Start () {
+        CurrentHealth = MaxHealth;
+	}
+
+    public void DecreaseHealth()
+    {
+        CurrentHealth -= 5f;
+        UpdateHealthBar();
+    }
+
+    public void IncreaseHealth()
+    {
+        CurrentHealth += 5f;
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        Bar.fillAmount = CurrentHealth / MaxHealth;
+    }
+}
